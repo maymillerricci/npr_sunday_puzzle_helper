05090335c2cc0789584b956316de54bf4259754d
May Miller-Ricci <maymillerricci@gmail.com>
2015-03-19 19:35:23 -0400

    require seed data for tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,4 +47,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:all) do
+    require "#{Rails.root}/db/seeds.rb"
+  end
 end
