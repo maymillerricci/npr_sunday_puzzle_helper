b67de74f566692a2b2611e51380bc9a0a87ebd09
May Miller-Ricci <maymillerricci@gmail.com>
2015-03-19 22:28:59 -0400

    add test for anagram with adding a letter

--- spec/features/anagram_spec.rb
+++ spec/features/anagram_spec.rb
@@ -41,6 +41,21 @@ describe "anagram plus" do
       expect(page).to have_text("liberiasudan")
     end
 
+    it "should show correct answer when adding a letter" do
+      visit new_anagram_path
+
+      select "Anagram", from: "anagram_tool"
+      fill_in "anagram_input_text", with: "r"
+      select "Capitals", from: "anagram_input_category"
+      select "Presidents", from: "anagram_output_category"
+      select "2", from: "anagram_output_num_words"
+
+      click_button "Anagram+!"
+
+      expect(page).to have_text("bucharest")
+      expect(page).to have_text("carterbush")
+    end
+
   end
 
 end
