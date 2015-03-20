ed862928514c3544162781232471eea6472ceec8
May Miller-Ricci <maymillerricci@gmail.com>
2015-03-19 22:45:42 -0400

    move categories with info into hash in helper

--- app/helpers/anagrams_helper.rb
+++ app/helpers/anagrams_helper.rb
@@ -1,2 +1,13 @@
 module AnagramsHelper
+  CATEGORIES = [
+    { name: "Countries",        model: Country,   primary_field: :name },
+    { name: "Country Capitals", model: Capital,   primary_field: :name },
+    { name: "States",           model: State,     primary_field: :name },
+    { name: "State Capitals",   model: State,     primary_field: :capital },
+    { name: "Presidents",       model: President, primary_field: :last_name }
+  ]
+
+  def category_names
+    CATEGORIES.map { |c| c[:name] }
+  end
 end
