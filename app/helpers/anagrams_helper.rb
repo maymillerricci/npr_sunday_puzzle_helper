57637b3b9da5f24e589a126f22e3e645bf2c12de
May Miller-Ricci <maymillerricci@gmail.com>
2015-03-21 19:54:30 -0700

    add animals to categories list

--- app/helpers/anagrams_helper.rb
+++ app/helpers/anagrams_helper.rb
@@ -3,10 +3,11 @@ module AnagramsHelper
 
   CATEGORIES = [
     { name: "Countries",        model: Country,   primary_field: :name },
-    { name: "World Capitals", model: Capital,   primary_field: :name },
+    { name: "World Capitals",   model: Capital,   primary_field: :name },
     { name: "States",           model: State,     primary_field: :name },
     { name: "State Capitals",   model: State,     primary_field: :capital },
-    { name: "Presidents",       model: President, primary_field: :last_name }
+    { name: "Presidents",       model: President, primary_field: :last_name },
+    { name: "Animals",          model: Animal,    primary_field: :name }
   ]
 
   def category_names
